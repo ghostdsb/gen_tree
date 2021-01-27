20e99896b8c381bf03b924b351a8000ae86a7085
dibyanshu <dibyanshu@gamezop.co>
2021-01-27 22:31:38 +0530

    first commit

---
+++ lib/gen_tree.ex
@@ -0,0 +1,18 @@
+defmodule GenTree do
+  @moduledoc """
+  Documentation for `GenTree`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GenTree.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/gen_tree_test.exs
@@ -0,0 +1,8 @@
+defmodule GenTreeTest do
+  use ExUnit.Case
+  doctest GenTree
+
+  test "greets the world" do
+    assert GenTree.hello() == :world
+  end
+end
